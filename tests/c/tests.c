33548dc5ad50ffebd6e9aebde5a7fd919ee49b62
Ger Hobbelt <ger@hobbelt.com>
2024-10-21 07:58:01 +0200

    fix compiler warnings.

--- tests/c/tests.c
+++ tests/c/tests.c
@@ -3,7 +3,7 @@
 
 // this just checks that we can compile successfully
 
-int main() {
+int main(void) {
   for(uint32_t x = 0; x < 1000000; ++x) assert(fastrange32(x,5) < 5);
   for(uint64_t x = 0; x < 1000000; ++x) assert(fastrange64(x,5) < 5);
 }
